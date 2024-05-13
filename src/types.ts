b31a4e8117ce7de25a7ac65ba402544833fb48b3
Jorg <1423625+jorgvm@users.noreply.github.com>
2024-05-13 14:26:45 +0200

    feat: Integrate Firebase
    - Set and fetch gameboard in Firebase
    - Move utilities to separate folder
    - Move type to seperate file
    - Update README

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+interface Player {
+  id: string;
+  name: string;
+}
+
+interface Room {
+  id: string;
+  owner?: string;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+  x: number;
+  y: number;
+}
+
+type Side = "top" | "right" | "bottom" | "left";
+
+type Gameboard = Room[];
+
+interface Game {
+  gameboard: Gameboard;
+  players: Player[];
+  gridWidth: number;
+  gridHeight: number;
+  status: "waiting" | "playing" | "finished";
+  turnId?: number;
+}
