5e3e5f3616a8649e9947c1a50bb76c59416d2731
Jorg <1423625+jorgvm@users.noreply.github.com>
2025-03-01 12:33:43 +0100

    chore: update sentry, firebase, uuid

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,29 +2,12 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-
-      // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
-      // spotlight: process.env.NODE_ENV === 'development',
-    });
+    await import("../sentry.server.config");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false,
-    });
+    await import("../sentry.edge.config");
   }
 }
+
+export const onRequestError = Sentry.captureRequestError;
